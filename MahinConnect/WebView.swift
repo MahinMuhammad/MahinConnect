194c8dd60744860af55e6730b1bd506ec1849fa8
MahinMuhammad <amimahinurrahman@gmail.com>
2023-06-13 11:45:39 +0600

    webview addd

--- MahinConnect/WebView.swift
+++ MahinConnect/WebView.swift
@@ -5,4 +5,40 @@
 //  Created by Md. Mahinur Rahman on 6/13/23.
 //
 
+/*
+ Copyright 2023 Md. Mahinur Rahman
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
 import Foundation
+import WebKit
+import SwiftUI
+
+struct WebView:UIViewRepresentable{
+    
+    let urlString: String?
+    
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let safeString = urlString{
+            if let url = URL(string: safeString){
+                let request = URLRequest(url: url)
+                uiView.load(request)
+            }
+        }
+    }
+}
