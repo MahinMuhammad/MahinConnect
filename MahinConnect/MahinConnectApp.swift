14cc867766e1b9041bacaa05d79c2b53378024ce
MahinMuhammad <amimahinurrahman@gmail.com>
2023-06-13 11:35:12 +0600

    Initial Commit

---
+++ MahinConnect/MahinConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MahinConnectApp.swift
+//  MahinConnect
+//
+//  Created by Md. Mahinur Rahman on 6/13/23.
+//
+
+import SwiftUI
+
+@main
+struct MahinConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
